0e9764db5fa0a3adb749ee33e3f1855340e948d2
Asadul Islam <asadul.santipur@gmail.com>
2026-07-25 20:32:17 +0300

    first commit

---
+++ problem1.js
@@ -0,0 +1,17 @@
+// without function
+// let number = 8
+// if (number %2===0) {
+//     console.log("even number")
+// } else{
+//     console.log("odd number")
+// }
+
+// with function
+function checkEvenOrOdd(number){
+if (number %2===0) {
+    return"even number";
+} else{
+    return"odd number";
+}
+}
+console.log(checkEvenOrOdd(544));
