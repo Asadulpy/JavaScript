31d245b2c33be3ab3d4535ea841f98a2bca1c4a2
Asadul Islam <asadul.santipur@gmail.com>
2026-07-26 14:29:46 +0300

    count word in a sentence

---
+++ problem8.js
@@ -0,0 +1,14 @@
+// count word in a sentence
+// let str = "I am a student";
+// let strSplitArr = str.split(" ");
+// console.log(strSplitArr.length);
+
+
+// with function
+function wordcount(str){
+ let strSplitArr = str.split(" ");
+  
+return strSplitArr.length
+}
+console.log(wordcount("mr Bean is a comedian"));
+
